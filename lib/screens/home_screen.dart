66347d7368f292e4df34eda1af0f266092bcc021
AwaisGill <trainingtuts@gmail.com>
2022-09-20 15:23:59 +0500

    darkmode working good

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'other_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User App'),
+        centerTitle: true,
+        elevation: 0,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.brightness_4),
+              onPressed: () {
+                Get.isDarkMode
+                    ? Get.changeTheme(ThemeData.light())
+                    : Get.changeTheme(ThemeData.dark());
+              })
+        ],
+      ),
       body: Center(
-        child: Text('data'),
+        child: ElevatedButton(
+          child: const Text('Go to Other Screen'),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const OtherScreen()));
+          },
+        ),
       ),
     );
   }
